f30fd1a784db339c2b3994e5d75d19122d579b9d
竹田永李 <ERI.TAKEDA@grp.tmnf.jp>
2022-05-31 15:17:44 +0900

    ページネーションを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -16,6 +16,10 @@ import javax.persistence.Table;
     @NamedQuery(
             name = "getAllMessages",
             query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            ),
+    @NamedQuery(//ページネーションのために、DBに何件データが入っているか知る
+            name = "getMessagesCount",
+            query = "SELECT COUNT(m) FROM Message AS m"
             )
 })
 @Table(name = "messages")
